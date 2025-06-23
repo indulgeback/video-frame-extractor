a524deeed1f34ca78748721e9ef909f8dae69a1f
LeviLiu <liuwenyu1937@outlook.com>
2025-06-23 19:32:55 +0800

    fix: 更新安装脚本以使用 pip3 安装依赖，并添加 .DS_Store 文件

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ TMP_DIR=$(mktemp -d)
 echo "正在下载 video-frame-extractor..."
 git clone https://github.com/indulgeback/video-frame-extractor.git $TMP_DIR
 cd $TMP_DIR
-pip install .
+pip3 install .
 echo "安装完成！你可以直接使用 frame-extractor 命令。"
